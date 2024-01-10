a97c2ae69f08a7dbb3289754f814298be657892f
Kurkov11 <kurkovsw@icloud.com>
2024-01-10 13:36:24 +0100

    Darken the background oclick menu

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
 document.getElementById('menu-icon').onclick = function(){
     document.getElementById('nav-bar').style.display = 'flex';
+    document.getElementById('black-box').style.display = 'block';
     document.getElementById('stop-scrolling').style.overflow = 'hidden';
 }
 document.getElementById('close-menu-icon').onclick = function(){
     document.getElementById('nav-bar').style.display = 'none';
+    document.getElementById('black-box').style.display = 'none';
     document.getElementById('stop-scrolling').style.overflow = 'visible';
 }
