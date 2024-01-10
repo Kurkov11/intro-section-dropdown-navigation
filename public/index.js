67dc8c757488b885db24692966c9441c0e2a43a7
Kurkov11 <kurkovsw@icloud.com>
2024-01-10 16:43:21 +0100

    Problematic place-sponsors go out of wrapper in mobile

--- public/index.js
+++ public/index.js
@@ -1,10 +1,40 @@
 document.getElementById('menu-icon').onclick = function(){
     document.getElementById('nav-bar').style.display = 'flex';
     document.getElementById('black-box').style.display = 'block';
-    document.getElementById('stop-scrolling').style.overflow = 'hidden';
 }
 document.getElementById('close-menu-icon').onclick = function(){
     document.getElementById('nav-bar').style.display = 'none';
     document.getElementById('black-box').style.display = 'none';
-    document.getElementById('stop-scrolling').style.overflow = 'visible';
+}
+
+let featuresExpanded = false;
+document.getElementById('features-click-area').onclick = function(){
+    if(featuresExpanded){
+        document.getElementById('todolist').style.display = 'none';
+        document.getElementById('reminders').style.display = 'none';
+        document.getElementById('callendar').style.display = 'none';
+        document.getElementById('planning').style.display = 'none';
+        featuresExpanded = false;
+    }else{
+        document.getElementById('todolist').style.display = 'block';
+        document.getElementById('reminders').style.display = 'block';
+        document.getElementById('callendar').style.display = 'block';
+        document.getElementById('planning').style.display = 'block';
+        featuresExpanded = true;
+    }
+}
+
+let companyExpanded = false;
+document.getElementById('company-click-area').onclick = function(){
+    if(companyExpanded){
+        document.getElementById('history').style.display = 'none';
+        document.getElementById('our-team').style.display = 'none';
+        document.getElementById('blog').style.display = 'none';
+        companyExpanded = false;
+    }else{
+        document.getElementById('history').style.display = 'block';
+        document.getElementById('our-team').style.display = 'block';
+        document.getElementById('blog').style.display = 'block';
+        companyExpanded = true;
+    }
 }
